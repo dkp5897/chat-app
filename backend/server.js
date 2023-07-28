42ea3ddf2a2f506821af2de5f6caa6b1d892dab1
Deepak kumar <102683400+dkp5897@users.noreply.github.com>
2023-07-28 09:37:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.get("/", (req, res) => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://air-talk.netlify.app",
     // credentials: true,
   },
 });
